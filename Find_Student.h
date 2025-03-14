a7fc2e8b4e32f73993acf87f6dbff1298c23153d
Волков Егор Ильич <egormatch31@gmail.com>
2025-03-14 17:43:06 +0300

    Update Find_Student.h

--- Find_Student.h
+++ Find_Student.h
@@ -3,27 +3,30 @@
 #include <string.h>
 
 
-#define FILE_NAME "stud.dat"
+#define NAME "stud.dat"
 
-void Find_Student(){
-
-    FILE *file = fopen(FILE_NAME, "rb");
+void Find_Student() {
+    FILE *file = fopen(NAME, "rb");
     if (!file) {
         perror("File opening error");
         return;
     }
-    char search[50];
-    printf("Enter the searching field (name/faculty/group) with enter: ");
-    scanf("%49s", search);
+    char request[50];
+    printf("Enter the searching field (name/faculty/group/GPA) with enter: ");
+    scanf("%49s", request);
+    
     Student s;
     int found = 0;
+    char gpa_str[10];
+    
     while (fread(&s, sizeof(Student), 1, file)) {
-        if (strstr(s.name, search) || strstr(s.faculty, search) || strstr(s.group, search)) {
+        snprintf(gpa_str, sizeof(gpa_str), "%.2f", s.GPA);
+        if (strstr(s.name, request) || strstr(s.faculty, request) || strstr(s.group, request) || strstr(gpa_str, request)) {
             printf("\nResults of searching: %s | %s | %s | %.2f\n", s.name, s.faculty, s.group, s.GPA);
             found = 1;
         }
     }
-    if (found == 0) {
+    if (!found) {
         printf("No results found\n");
     }
     fclose(file);
