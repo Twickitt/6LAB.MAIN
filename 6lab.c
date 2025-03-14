d25b8e4b2d7c9ab46fb576ff99a6f5939ece0ba8
Волков Егор Ильич <egormatch31@gmail.com>
2025-03-14 15:31:32 +0300

    Update 6lab.c

--- 6lab.c
+++ 6lab.c
@@ -4,14 +4,33 @@
 #include "Find_Student.h"
 #include "Edit_Student.h"
 #include "Display_Student.h"
+#define  N 1
 
-#define FILE_NAME "stud.dat"
+#define NAME "stud.dat"
 
+#define USE_PREDEFINED_DATA  // Закомментируйте, если хотите ввод вручную
 
 void (*menu[])(void) = {Add_Student, Del_Student, Find_Student, Edit_Student, Displ_Student};
 
 
 int main(){
+
+    #if N == 5
+    FILE *file = fopen(NAME, "w");
+    fseek(file, 0, SEEK_SET);
+    Student predefinedStudents[] = {
+        {"Alice Johnson", "Engineering", "E21", 3.8},
+        {"Bob Smith", "Mathematics", "M2", 3.5},
+        {"Charlie Brown", "Physics", "245", 3.9}
+    };
+    fwrite(predefinedStudents, sizeof(Student), 3, file);
+    fclose(file);
+
+
+
+    #endif
+
+
 
     int choice;
 
@@ -35,4 +54,3 @@ int main(){
     } while (choice != 0);
     return 0;
 }
-
